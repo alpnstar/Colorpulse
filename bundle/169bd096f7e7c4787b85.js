2903c9d3e78fec6e604edb0c2ec2e2951491c3bf
Satiyadzhiev Daud Adjakavovich <alpn.ds@mail.ru>
2023-03-14 22:32:35 +0300

    webpack

---
+++ bundle/169bd096f7e7c4787b85.js
@@ -0,0 +1,1 @@
+import"./../index.html";import"./../styles/style.css";
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const mode = process.env.NODE_ENV || 'development';
+const devMode = mode === 'development';
+const target = devMode ? 'web' : 'browserslist';
+const devtool = devMode ? 'source-map' : undefined;
+
+
+module.exports = {
+    mode,
+    devtool,
+    target,
+    entry: path.resolve(__dirname, './src/js/script.js'),
+    output: {
+        path: path.resolve(__dirname, './bundle'),
+        clean: true,
+        filename: 'bundle.[contenthash].js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'),
+            minify: false,
+        }),
+        new MiniCssExtractPlugin({
+            
+        }),
+    ],
+    module: {
+        rules: [
+          {
+            test: /\.html$/i,
+            loader: "html-loader",
+            options: {
+                minimize: false,
+            }
+          },
+          {
+            test: /\.css$/i,
+            use: [devMode ? "style-loader" : MiniCssExtractPlugin.loader, "css-loader"],
+          },
+        ],
+      },
+}
