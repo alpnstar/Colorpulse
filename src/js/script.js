ae2fba6ba0c6bc9448463223fcf277272d9b1781
Satiyadzhiev Daud Adjakavovich <alpn.ds@mail.ru>
2023-03-19 18:11:36 +0300

    slider ready

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,32 @@
 import "./../index.html";
 import "./../scss/style.scss";
 import "@babel/polyfill";
+
+slider(document.querySelectorAll('.reviews__item'),
+    1054,
+    document.querySelector('.reviews__slider-buttons--right'),
+    document.querySelector('.reviews__slider-buttons--left'))
+
+
+function slider(sliderItem, wrapperWidthPX, buttonNext, buttonPrev) {
+    let itemCount = sliderItem.length;
+    let sliderStep = 0;
+
+    buttonNext.onclick = () => {
+        if (sliderStep != itemCount - 1) {
+            sliderStep++;
+            sliderItem.forEach(item => {
+                item.style.transform = `translate(${-wrapperWidthPX * sliderStep}px)`;
+            })
+        }
+    }
+    buttonPrev.onclick = () => {
+        if (sliderStep != 0) {
+            sliderStep--;
+            sliderItem.forEach(item => {
+                item.style.transform = `translate(${-wrapperWidthPX * sliderStep}px)`;
+            })
+        }
+    }
+}
+
